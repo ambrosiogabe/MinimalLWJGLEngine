5f31f07c8af73139c37cc5aecc7a9b08e079bbef
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-08-19 11:27:07 +0530

    Update MouseListener.java
    C-Style array declaration changed to the java standard.

--- src/main/java/eventHandlers/MouseListener.java
+++ src/main/java/eventHandlers/MouseListener.java
@@ -7,7 +7,7 @@ public class MouseListener {
     private static MouseListener instance;
     private double scrollX, scrollY;
     private double xPos, yPos, lastY, lastX;
-    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean[] mouseButtonPressed = new boolean[3];
     private boolean isDragging;
 
     private MouseListener() {
